cc0895cdbc47601e48fe1e3f984e25ca35698e13
Nikita Korobeinikov <korobeinikov@surfstudio.ru>
2021-03-20 18:50:24 +0400

    master: add stub unit tests

---
+++ Example/KinescopeExampleTests/Tests.swift
@@ -0,0 +1,18 @@
+//
+//  Tests.swift
+//  KinescopeExampleTests
+//
+//  Created by Никита Коробейников on 20.03.2021.
+//
+
+import XCTest
+@testable import KinescopeExample
+
+/// Just stub for tests
+final class Tests: XCTestCase {
+
+    func stubTest() {
+
+    }
+
+}
---
+++ Sources/KinescopeSDKTests/Tests.swift
@@ -0,0 +1,17 @@
+//
+//  Tests.swift
+//  KinescopeSDKTests
+//
+//  Created by Никита Коробейников on 20.03.2021.
+//
+
+import XCTest
+
+/// Just stub for tests
+final class Tests: XCTestCase {
+
+    func stubTest() {
+
+    }
+
+}
