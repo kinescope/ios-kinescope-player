87b178bb0844d6f671e2e93ada11c89eaf32be83
Nikita Korobeinikov <korobeinikov@surfstudio.ru>
2021-03-22 11:05:09 +0400

    develop: add stub file with issues for linter

---
+++ Sources/KinescopeSDK/Protocols/KinescopePlayer.swift
@@ -0,0 +1,24 @@
+//
+//  KinescopePlayer.swift
+//  KinescopeSDK
+//
+//  Created by Никита Коробейников on 22.03.2021.
+//
+
+public protocol KinescopePlayer {
+
+    func set(apiKey: String)
+
+
+
+}
+
+struct STUBSTRUCTWITHISSUESANDERRORSFORLINTER {
+    let a: Int
+
+        let b: String
+
+
+    let abds: Error
+
+}
