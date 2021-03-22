13bec1f249fef46b11acf87b341ddedfeed5c3f0
Nikita Korobeinikov <korobeinikov@surfstudio.ru>
2021-03-22 16:17:32 +0400

    feature/KIN-00/architecture: architecture- add minimal Player interface

--- Sources/KinescopeSDK/Protocols/KinescopePlayer.swift
+++ Sources/KinescopeSDK/Protocols/KinescopePlayer.swift
@@ -5,7 +5,15 @@
 //  Created by Никита Коробейников on 22.03.2021.
 //
 
+/// Control protocol for player
 public protocol KinescopePlayer {
 
-    func set(apiKey: String)
+    /// - parameter video_id: Id of concrete video. For example from [GET Videos list](https://documenter.getpostman.com/view/10589901/TVCcXpNM)
+    init(video_id: String)
+
+    /// Start playing of video
+    func play()
+
+    /// Stop playing of video
+    func stop()
 }
