2461ad185da40a7e9c82f862e65c2ad12a8b127c
Anton Dryakhlykh <drxlx@ya.ru>
2021-03-24 15:57:00 +0300

    update models

--- Sources/KinescopeSDK/Models/Videos/KinescopeVideo.swift
+++ Sources/KinescopeSDK/Models/Videos/KinescopeVideo.swift
@@ -9,11 +9,15 @@ public struct KinescopeVideo: Codable {
     public let id: String
     public let projectId: String
     public let version: Int
+    public let title: String
+    public let description: String
     public let status: String
     public let progress: Int
     public let duration: Double
     public let assets: [KinescopeVideoAsset]
-    public let poster: KinescopeVideoPoster?
-    public let subtitlesEnabled: Bool
+    public let chapters: KinescopeVideoChapter
+    public let poster: KinescopeVideoPoster
+    public let additionalMaterials: [KinescopeVideoAdditionalMaterial]
+    public let subtitles: [KinescopeVideoSubtitle]
     public let hlsLink: String
 }
