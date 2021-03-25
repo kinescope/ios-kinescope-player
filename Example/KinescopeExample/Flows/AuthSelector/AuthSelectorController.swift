dfca98646845a4c30347e56a9a0f9ee5409ba185
Nikita Korobeinikov <korobeinikov@surfstudio.ru>
2021-03-25 11:30:55 +0400

    feature/KIN-28/auth_key_selector: auth_key_selector- add auth selector VC to storyboard

--- Example/KinescopeExample/Flows/AuthSelector/AuthSelectorController.swift
+++ Example/KinescopeExample/Flows/AuthSelector/AuthSelectorController.swift
@@ -7,3 +7,51 @@
 
 import UIKit
 import ReactiveDataDisplayManager
+
+final class AuthSelectorController: UIViewController {
+
+    @IBOutlet private weak var tableView: UITableView!
+    @IBOutlet private weak var emptyView: UIView!
+
+    // MARK: - Private Properties
+
+    private lazy var adapter = tableView.rddm.baseBuilder
+        .add(plugin: .selectable())
+        .build()
+
+    // MARK: - UIViewController
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        title = "Who are you?"
+        loadUsers()
+    }
+
+}
+
+// MARK: - Private Methods
+
+private extension AuthSelectorController {
+
+    func loadUsers() {
+
+        let users = ConfigStorage.read()
+
+        fillAdapter(with: users)
+
+        emptyView.isHidden = !users.isEmpty
+
+    }
+
+    func fillAdapter(with users: [User]) {
+
+        adapter.clearCellGenerators()
+
+        let generators = users.map { UserCell.rddm.baseGenerator(with: $0) }
+
+        adapter.addCellGenerators(generators)
+
+        adapter.forceRefill()
+    }
+
+}
