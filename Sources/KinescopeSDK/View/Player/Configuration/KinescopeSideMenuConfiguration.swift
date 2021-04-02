bc34a0fab414475eae2e9734cb8c877af1883943
Nikita Korobeinikov <korobeinikov@surfstudio.ru>
2021-04-02 19:08:44 +0400

    feature/KIN-44: KIN-44 add resources for sidemenu

---
+++ Sources/KinescopeSDK/View/Player/Configuration/KinescopeSideMenuConfiguration.swift
@@ -0,0 +1,8 @@
+//
+//  KinescopeSideMenuConfiguration.swift
+//  KinescopeSDK
+//
+//  Created by Никита Коробейников on 02.04.2021.
+//
+
+import Foundation
---
+++ Sources/KinescopeSDK/View/Player/SideMenu/SideMenu.swift
@@ -0,0 +1,17 @@
+//
+//  SideMenu.swift
+//  KinescopeSDK
+//
+//  Created by Никита Коробейников on 02.04.2021.
+//
+// swiftlint:disable implicitly_unwrapped_optional
+
+import UIKit
+
+final class SideMenu: UIView {
+
+    private weak var tableView: UITableView!
+
+    
+
+}
