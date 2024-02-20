61c50d595ffe3fa91661f277ebc77ee20ba4099f
Nikita Korobeinikov <nikita.korobeinikov@devexperts.com>
2024-02-20 11:51:33 +0400

    add video example with DRM protection

--- Example/KinescopeExample/Flows/VideoViewController.swift
+++ Example/KinescopeExample/Flows/VideoViewController.swift
@@ -10,7 +10,13 @@ final class VideoViewController: UIViewController {
     // MARK: - Private properties
 
     // Change this id to your video id from dashboard. This one is free video for demo of SDK only.
-    private let videoId = "9L8KmbNuhQSxQofn5DR4Vg"
+    private let videoId: String = {
+#if targetEnvironment(simulator)
+        "9L8KmbNuhQSxQofn5DR4Vg"
+#else
+        "200660125"
+#endif
+    }()
     private var player: KinescopePlayer?
 
     // MARK: - Lifecycle
