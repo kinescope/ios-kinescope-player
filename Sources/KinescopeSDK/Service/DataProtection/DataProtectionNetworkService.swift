eccb0ca6d70fc0ef1f34664b3731d1f24e0c6936
Nikita Korobeinikov <nikita.korobeinikov@devexperts.com>
2024-02-21 14:33:02 +0400

    fix executions of loading tasks

--- Sources/KinescopeSDK/Service/DataProtection/DataProtectionNetworkService.swift
+++ Sources/KinescopeSDK/Service/DataProtection/DataProtectionNetworkService.swift
@@ -14,6 +14,9 @@ final class DataProtectionNetworkService: DataProtectionService {
     private let transport: Transport
     private let config: KinescopeConfig
 
+    private let executionQueue = DispatchQueue.global(qos: .utility)
+    private let errorQueue = DispatchQueue.main
+
     // MARK: - Lifecycle
 
     init(transport: Transport, config: KinescopeConfig) {
@@ -24,24 +27,38 @@ final class DataProtectionNetworkService: DataProtectionService {
     // MARK: - Public Methods
 
     func getCert(for videoId: String, completion: @escaping (Result<Data, Error>) -> Void) {
-        do {
-            let url = URL(string: "\(config.keyServer)\(videoId)/certificate/fairplay")!
-            let data = try Data(contentsOf: url)
-            completion(.success(data))
-        } catch let error {
-            completion(.failure(error))
+        executionQueue.async { [weak self] in
+            guard let self else {
+                return
+            }
+            do {
+                let url = URL(string: "\(config.keyServer)\(videoId)/certificate/fairplay")!
+                let data = try Data(contentsOf: url)
+                completion(.success(data))
+            } catch let error {
+                errorQueue.async {
+                    completion(.failure(error))
+                }
+            }
         }
     }
 
     func getContentKey(for videoId: String, body: DataProtectionRequestBody, completion: @escaping (Result<DataProtectionResponse, Error>) -> Void) {
-        do {
-            let request = try RequestBuilder(path: "\(config.keyServer)\(videoId)/acquire/fairplay",
-                                             method: .post)
-                .build(body: body)
+        executionQueue.async { [weak self] in
+            guard let self else {
+                return
+            }
+            do {
+                let request = try RequestBuilder(path: "\(config.keyServer)\(videoId)/acquire/fairplay",
+                                                 method: .post)
+                    .build(body: body)
 
-            transport.performFetch(request: request, completion: completion)
-        } catch let error {
-            completion(.failure(error))
+                transport.performFetch(request: request, completion: completion)
+            } catch let error {
+                errorQueue.async {
+                    completion(.failure(error))
+                }
+            }
         }
     }
 
